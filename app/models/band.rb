ef4f58798a72248b9eaa398809e92edec888223a
sadrightnow <froelichperry1@gmail.com>
2024-11-16 12:20:46 -0700

    upload limit

--- app/models/band.rb
+++ app/models/band.rb
@@ -3,5 +3,16 @@ class Band < ApplicationRecord
   has_and_belongs_to_many :posts
   has_many :shows # Assuming shows are associated with bands
   has_and_belongs_to_many :genres
+
   validates :band_name, presence: true
+  validate :profile_picture_size
+
+  private
+
+  # Custom validation method to check file size
+  def profile_picture_size
+    if profile_picture.attached? && profile_picture.byte_size > 5.megabytes
+      errors.add(:profile_picture, "should be less than 5MB")
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,29 @@ class Post < ApplicationRecord
   has_and_belongs_to_many :genres
   has_and_belongs_to_many :bands
 
-  #def genre
-  #end
+  validate :image_size, :show_poster_size
+
+  private
+
+  # Custom validation method to check file size for images
+  def image_size
+    if image.attached?
+      image.each do |img|
+        if img.byte_size > 5.megabytes
+          errors.add(:image, "each file should be less than 5MB")
+        end
+      end
+    end
+  end
+
+  # Custom validation method to check file size for show posters
+  def show_poster_size
+    if show_poster.attached?
+      show_poster.each do |poster|
+        if poster.byte_size > 5.megabytes
+          errors.add(:show_poster, "each file should be less than 5MB")
+        end
+      end
+    end
+  end
 end
