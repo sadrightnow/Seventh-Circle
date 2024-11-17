a74cbc31615ffbfac2498fea05e0f8a44d812144
sadrightnow <froelichperry1@gmail.com>
2024-11-17 12:35:39 -0700

    fixed post.css

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.action_controller.perform_caching = false
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
